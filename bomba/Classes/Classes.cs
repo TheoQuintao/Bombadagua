7c0c03e7d154e36b79bc8685e52eb741324585fc
Theo Quintao <0000900198@senaimgaluno.com.br>
2025-05-15 10:54:23 -0300

    Add Torneira

--- bomba/Classes/Classes.cs
+++ bomba/Classes/Classes.cs
@@ -2,12 +2,24 @@ using System.Security.Cryptography;
 
 namespace Classes
 {
+    public class Bomba
+    {
+        public bool Estado { get; set; }
+        public static double Vazao { get; set; }
+        public double TempoFuncionamento { get; set; }
+    }
+    public class Torneira
+    {
+        public bool Estado { get; set; }
+        public static double Vazao { get; set; }
+        public double TempoFuncionamento { get; set; }
+    }
     public static class Reservatorio
     {
         public static double Capacidade { get; set; }
         public static double NivelAtual { get; set; }
-        public static  double Saida => Bomba.Vazao;
-        public static double Entrada { get; set; }  
+        public static double Saida => Bomba.Vazao;
+        public static double Entrada => Torneira.Vazao;
     }
     public class Caixa
     {
@@ -16,10 +28,5 @@ namespace Classes
         public double Saida { get; set; }
         public double Entrada => Reservatorio.Saida;
     }
-    public class Bomba
-    {
-        public bool Estado { get; set; }
-        public static double Vazao { get; set; }
-        public double TempoFuncionamento { get; set; }
-    }
+    
 }
